cdf9ddfc376f64515bb733ec3a288fb832a0d484
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-03-08 22:04:25 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-  var movie = [
+  var movie1 = [
       {a:"https://i.ytimg.com/vi/f_vbAtFSEc0/maxresdefault.jpg"},
       {a:"https://i0.wp.com/neplych.com/wp-content/uploads/2022/02/Gangubai-Kathiawadi-Movie.jpg?fit=1440%2C960&ssl=1"},
       {a:"https://www.eaglesvine.com/wp-content/uploads/2021/07/1625665142_Bhuj-Motion-Poster-On-Hows-The-Hype-Blockbuster-Or-Lacklustre-780x470.jpg"},
@@ -6,24 +6,24 @@
       {a:"https://www.koimoi.com/wp-content/new-galleries/2021/11/satyameva-jayate-2-trailer-2-ft-john-abraham-on-hows-the-hype-001.jpg"}
   
   ]
-    localStorage.setItem("movie" ,JSON.stringify(movie))
-     var movie=JSON.parse(localStorage.getItem("movie"))
-    console.log(movie)
+    localStorage.setItem("movie1" ,JSON.stringify(movie1))
+     var movie1=JSON.parse(localStorage.getItem("movie1"))
+    console.log(movie1)
    let slideshow =document.getElementById("slideshow");
    let i=1;
    let img=document.createElement("img");
       img.setAttribute("id","img")
-       img.src=movie[0].a;
+       img.src=movie1[0].a;
        slideshow.append(img)
     setInterval(function (){
-       if(i==movie.length){
+       if(i==movie1.length){
            i=0;
        }
-       let movies=movie[i];
+       let movies1=movie1[i];
        slideshow.innerHTML=null;
        let img=document.createElement("img");
        img.setAttribute("id","img")
-       img.src=movies.a;
+       img.src=movies1.a;
        slideshow.append(img)
        i++
     },1000)
