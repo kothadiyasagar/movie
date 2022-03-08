04b3d4c464e98d463fd69ce8e5c39afc48da93ed
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-03-08 23:44:59 +0530

    movies poster grid

--- scripts/index.js
+++ scripts/index.js
@@ -27,3 +27,38 @@
        slideshow.append(img)
        i++
     },1000)
+
+      var movies = [
+          {image:"https://www.deccanherald.com/sites/dh/files/articleimages/2021/12/17/file7ivjchsfnw1f6gj8eay-1061813-1639743521.jpg", name:"Pushpa", action:"Action,Drama",rate:"9"},
+          {image:"https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX380_CR0,0,380,562_.jpg", name:"The Batman", action:"Action,Drama",rate:"8.5"},
+          {image:"https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i", name:"The Godfather", action:"Crime,Drama",rate:"9.2"},
+          {image:"https://www.imdb.com/title/tt0317248/mediaviewer/rm1021136128/?ref_=tt_ov_i", name:"City of God", action:"Crime,Drama",rate:"8.3"},
+          {image:"https://m.media-amazon.com/images/M/MV5BYmJmM2Q4NmMtYThmNC00ZjRlLWEyZmItZTIwOTBlZDQ3NTQ1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UX380_CR0,2,380,562_.jpg", name:"life Is Beautiful", action:"Crime,Drama,Romance,War",rate:"8"},
+          {image:"https://m.media-amazon.com/images/M/MV5BOWY0MzZkN2QtODI1MC00Nzg0LWE3YmUtNTNhMWU4YTFjOTA4XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_QL75_UY562_CR21,0,380,562_.jpg", name:" Bhuj: The Pride of India", action:"Crime,Drama,Romance,War",rate:"8.2"},
+          {image:"https://m.media-amazon.com/images/M/MV5BMTYzOTE2NjkxN15BMl5BanBnXkFtZTgwMDgzMTg0MzE@._V1_QL75_UY562_CR4,0,380,562_.jpg", name:"PK", action:"Comedy,Drama,Sci-Fi",rate:"9.5"},
+          {image:"https://m.media-amazon.com/images/M/MV5BNTkyOGVjMGEtNmQzZi00NzFlLTlhOWQtODYyMDc2ZGJmYzFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_QL75_UY562_CR5,0,380,562_.jpg", name:"3 Idiots", action:"Crime,Drama",rate:"9.6"},
+          {image:"https://m.media-amazon.com/images/M/MV5BMTY1Nzg4MjcwN15BMl5BanBnXkFtZTcwOTc1NTk1OQ@@._V1_QL75_UY562_CR5,0,380,562_.jpg", name:"Bhaag Milkha Bhaag", action:"Biography,Drama,Sport",rate:"8.7"}
+
+      ]
+      localStorage.setItem("movies" ,JSON.stringify(movies))
+      var movies=JSON.parse(localStorage.getItem("movies"))
+      console.log(movies)
+      display(movies)
+      function display(data){
+        document.querySelector("#movies").innerHTML=""
+        movies.map(function(elem,index){
+           var div= document.createElement("div")
+         div.setAttribute("id","div1")
+         var img = document.createElement("img");
+         img.setAttribute("src", elem.image);
+         img.setAttribute("id","img")
+         var div2= document.createElement("div")
+         div.setAttribute("id","div2")
+           var name=document.createElement("h5")
+           name.innerText=elem.name;
+           div.append(name)
+           document.querySelector("#movies").append(div)
+        })
+
+      }
+  
