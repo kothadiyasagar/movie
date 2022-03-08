789e9b630b824bed74e99238ac9db9652df59056
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-03-08 16:42:41 +0530

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,30 +1,51 @@
 var s= JSON.parse(localStorage.getItem("sign"));
   console.log(s);
 
-  document.querySelector("#aform").addEventListener("submit", submit);
-
-  var form = document.querySelector("#aform");
+    document.querySelector("#afrom").addEventListener("submit", fromdata)
+    var form = document.querySelector("#aform")
+     function fromdata(event){
+      event.preventDefault();
+      var email=document.querySelector("#email").value;
+      var pass=document.querySelector("#pass").value;
+      console.log(pass,email)
+       var c=0;
+       for(let i=0; i<s.length; i++){
+        if (s[i].email == email && s[i].pass == pass) {
+                alert("login successful.")
+                c++
+              window.location.href = "index.html";
+               
+               }
+              } 
+                if(c==0) {
+                alert("try again")
+              
+             }
+       }
+     
+  // var form = document.querySelector("#aform").value;
+  // console.log(form)
 
   //console.log(form.user.value);
   // form.id.value
-  function submit(event) {
-    event.preventDefault();
+  // function submit(event) {
+  //   event.preventDefault();
 
-    var email = form.email.value;
-    var pass = form.pass.value;
-    console.log(email, pass);
+  //   var email = form.email.value;
+  //   var pass = form.pass.value;
+  //   console.log(email, pass);
 
-    var c=0
-    for (var i = 0; i < regdUsers.length; i++) {
-      if (s[i].email == email && s[i].pass == pass) {
-        alert("login successful.")
-        window.location.href = "index.html";
-        c++
-      }
-      } 
-       if(c==0) {
-        alert("try again")
+  //   var c=0
+  //   for (var i = 0; i < regdUsers.length; i++) {
+  //     if (s[i].email == email && s[i].pass == pass) {
+  //       alert("login successful.")
+  //       window.location.href = "index.html";
+  //       c++
+  //     }
+  //     } 
+  //      if(c==0) {
+  //       alert("try again")
       
-      }
+  //     }
     
-  }
+  // }
